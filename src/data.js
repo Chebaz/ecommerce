9739e2588e135394cb81ac6e865798aee0eab8d9
Simone Checco <simone.checco1995@gmail.com>
2023-07-13 17:54:02 +0200

    navbar and slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "src/image/img1.png",
+    title: "SUMMAR SALE",
+    desc: "Don't Compromise on style! Get flat 30% OFF for new arrivals",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "src/image/img1.png",
+    title: "WINTER SALE",
+    desc: "Don't Compromise on style! Get flat 30% OFF for new arrivals",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "src/image/img1.png",
+    title: "POPOLAR SALE",
+    desc: "Don't Compromise on style! Get flat 30% OFF for new arrivals",
+    bg: "fbf0f4",
+  },
+];
